6a8aee76122992bb63a1183ae41ddbf22a2b0f8a
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-11-26 22:30:57 +0200

    Improve testing setup for gradle
    Signed-off-by: Alexandru-Cristian Sandu <alecsandu678@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
     id("java")
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
 group = "com.jbase"
 version = "1.0-SNAPSHOT"
 
@@ -13,6 +21,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+testing {
+    suites {
+        named<JvmTestSuite>("test") {
+            useJUnitJupiter("6.0.1")
+        }
+    }
+}
 }
 
 tasks.test {
